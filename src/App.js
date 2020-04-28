2d51351aa946880fed27a6472f7b0c8f6d564d89
muhammeta7 <muhammeta7@gmail.com>
2020-04-28 16:05:19 -0400

    added some basic routing to navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import UserCards from "./components/usercards/UserCards";
 import Navigation from "./components/navigation/Navigation";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/home/Home";
 
 function App() {
@@ -10,8 +10,11 @@ function App() {
     <Router>
       <div className="App">
         <Navigation />
-        <Route path="/" component={Home} />
-        <UserCards title="Github Cards" />
+        <Switch>
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/add/card" component={UserCards} />
+        </Switch>
+        {/* <UserCards title="Github Cards" /> */}
       </div>
     </Router>
   );
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
     <Navbar expand="lg" variant="dark" bg="dark">
-      <Navbar.Brand href="#home">GitHub Profiles</Navbar.Brand>
+      <Navbar.Brand>GitHub Profiles</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Link to="/">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Link>
+          <Link to="/add/card" className="navLink">
+            <Nav.Link href="#link">Add New Card</Nav.Link>
+          </Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item>Action</NavDropdown.Item>
             <NavDropdown.Item>Another action</NavDropdown.Item>
